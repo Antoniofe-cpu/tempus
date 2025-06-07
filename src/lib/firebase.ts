b82f7aaae9a118bd956d3f55105551a0ff92c95e
Antoniofe-cpu <ferrara.antonio08@gmail.com>
2025-06-07 19:21:54 +0200

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,13 @@ import { getFirestore, type Firestore } from 'firebase/firestore';
 // ATTENZIONE: Sostituisci questi valori con le tue credenziali Firebase!
 // Puoi trovarle nella console Firebase > Impostazioni progetto > Generali > Le tue app > Configurazione SDK.
 const firebaseConfig = {
-  apiKey: "TUO_API_KEY",
-  authDomain: "TUO_AUTH_DOMAIN",
-  projectId: "TUO_PROJECT_ID", // Assicurati che questo sia il Project ID corretto!
-  storageBucket: "TUO_STORAGE_BUCKET",
-  messagingSenderId: "TUO_MESSAGING_SENDER_ID",
-  appId: "TUO_APP_ID",
+  apiKey: "AIzaSyAN0v1Fq9mDOZ8-9CKlO99o50mCrS6ByiY",
+  authDomain: "tempus-concierge.firebaseapp.com",
+  projectId: "tempus-concierge", // Assicurati che questo sia il Project ID corretto!
+  storageBucket: "tempus-concierge.firebasestorage.app",
+  messagingSenderId: "544608215639",
+  appId: ":544608215639:web:099bf420d083fa9f64f912",
+measurementId: "G-3PBXV9TVJG"
   // measurementId: "TUO_MEASUREMENT_ID" // Opzionale, se usi Google Analytics
 };
 
