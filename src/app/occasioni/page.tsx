0089780c3f5057e87dddc09c8f3e3eb4a66b7f62
Antonio Ferrara <ferrara.antonio08@gmail.com>
2025-06-07 14:42:19 +0000

    ok concludiamo la funzione.. rendiamo collegata con la apgina lato clien

--- src/app/occasioni/page.tsx
+++ src/app/occasioni/page.tsx
@@ -38,9 +38,9 @@ export default function OccasioniPage() {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        <div role="list" className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
           {watchesData.map((watch) => (
-            <WatchCard key={watch.id} watch={watch} />
+            <WatchCard key={watch.id} watch={watch} /> 
           ))}
         </div>
         
